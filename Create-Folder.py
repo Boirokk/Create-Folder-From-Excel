4b313dd926230b7eda4f9f7a2cda362c36bdb0f0
Boirokk <chadzx10@gmail.com>
2016-05-13 15:41:52 -0400

    Added correct file path

--- Create-Folder.py
+++ Create-Folder.py
@@ -1,8 +1,8 @@
 import os, xlrd
 
 # Constant for EXCEL dirpath
-EXCEL = r"\\T13d-02\c$\Users\jblaner\Desktop\ESTHEC CUT OFFS.xlsx"
-FOLDER_LOCATION = r"C:\Users\cczilli\Desktop\New folder\test-t"
+EXCEL = r"C:\Users\cczilli\Desktop\Book1.xlsx"
+FOLDER_LOCATION = r"S:\_TDS\TDS JOB MASTER FILES"
 
 # Create an object for workbook and sheet
 workbook = xlrd.open_workbook(EXCEL)
@@ -10,15 +10,15 @@ sheet = workbook.sheet_by_index(0)
 
 # Iterate through excel sheet
 for row in range(sheet.nrows):
-    folder_front = sheet.cell_value(row,0)
-    folder_back = sheet.cell_value(row,1)
-    print(str(folder_front) + ' - ' + folder_back
 
-    # # Create directory with excel data
-    # try:
-    #     os.mkdir(FOLDER_LOCATION)
-    # except FileExistsError:
-    #     pass
+    try:
+        # Get excel data
+        folder_front = str(sheet.cell_value(row, 0))
+        folder_back = sheet.cell_value(row, 1)
+        # Create directory with excel data
+        os.mkdir(FOLDER_LOCATION + '\\' + folder_front + ' - ' + folder_back)
+    except FileExistsError:
+        continue
 
 
 
