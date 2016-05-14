c9ee4c5f9521866b3108bcecd74ecea796c7ec91
Boirokk <chadzx10@gmail.com>
2016-05-14 09:03:37 -0400

    Refactor Code
    Take the .0 out of the file name.

--- Create Master File.py
+++ Create Master File.py
@@ -1,9 +1,9 @@
 import os, xlrd
 
 # Constant for EXCEL dirpath
-EXCEL = r"C:\Users\cczilli\Desktop\Book1.xlsx"
-FOLDER_LOCATION = r"S:\_TDS\TDS JOB MASTER FILES\Master File Builder"
-#FOLDER_LOCATION = r"C:\Users\cczilli\Desktop\New folder"
+EXCEL = r"C:\Users\Chad\Desktop\Book1.xlsx"
+#FOLDER_LOCATION = r"S:\_TDS\TDS JOB MASTER FILES\Master File Builder"
+FOLDER_LOCATION = r"C:\Users\Chad\Desktop\New folder"
 
 # Create an object for workbook and sheet
 workbook = xlrd.open_workbook(EXCEL)
@@ -14,10 +14,14 @@ for row in range(sheet.nrows):
 
     try:
         # Get excel data
-        folder_front = int(sheet.cell_value(row, 0))
-        folder_back = sheet.cell_value(row, 1)
+        folder_front = str(sheet.cell_value(row, 0))
+        if '.0' in folder_front:
+            folder_front = folder_front.replace('.0','')
+
+        #folder_front = str(folder_front)
+        folder_back = str(sheet.cell_value(row, 1))
         # Create directory with excel data
-        os.mkdir(FOLDER_LOCATION + '\\' + str(folder_front) + '-' + folder_back.upper())
+        os.mkdir(FOLDER_LOCATION + '\\' + folder_front + '-' + folder_back.upper())
     except FileExistsError:
         continue
 
